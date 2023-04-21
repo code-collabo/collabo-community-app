6c59a51f9939d62774007c902cadfa6d81a5caee
Ifycode <marychord@gmail.com>
2023-04-21 19:00:35 +0100

    Restructure css in preparation for collabo-css building blocks development

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@/styles/app.globals.css';
+import '@/styles/app.base.globals.css';
+import '@/styles/app.main.globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
