839e2d27417b9d6ec1f67758963aec2adce47f2b
David <doladepo128@gmail.com>
2023-06-15 09:13:56 +0100

    Revert "Designed the layout of project and Internship pages."
    This reverts commit 7981b9422384af31b0ba4872f3b1824843972532.

--- client/src/pages/community/projects.tsx
+++ client/src/pages/community/projects.tsx
@@ -4,10 +4,6 @@ import Head from 'next/head';
 import { Project } from './../../model/project';
 import ProjectCard from "../../components/ProjectCard";
 import styles from "../../styles/modules/project.module.css";
-import Image from 'next/image';
-import Imageframe from "../../../public/Frame.svg";
-import arrowdown from "../../../public/Vectordown.svg";
-
 
 export default function ProjectsPage() {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -46,20 +42,15 @@ useEffect(() => {
         
         </div>
         <div className={styles.filters}>
-         <h3>Filter by:</h3> 
-         <ul>Interest <Image src={arrowdown} width={0} height={0} alt='down button' />   </ul>
-         <ul>Skill Set <Image src={arrowdown} width={0} height={0} alt='down button' />  </ul>
+         <h3>Filter By:</h3> 
+         <ul>Interest</ul>
+         <ul>Skill Set</ul>
         </div>
         </div>
-
-        <div className={styles.backgroundImage}>
-          <Image className={styles.image} src={Imageframe} width={400} height={100} alt='frame of picture' />
-        </div>
-
         <div className={styles.Cards}>
-        {/* {projects.map((project) => ( 
+        {projects.map((project) => ( 
         <ProjectCard  project={project} key={project._id}  />
-        ))} */}
+        ))}
         </div>
         </div>
         </>
