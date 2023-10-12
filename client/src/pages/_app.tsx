6f6ed27e97c35c6e8a66608760b2a3e23e1d04c8
Ifycode <marychord@gmail.com>
2023-10-12 23:24:42 +0100

    upgrade Nextjs app & restore app to favorable previous point to start afresh

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import '@/styles/app.base.globals.css';
 import '@/styles/app.main.globals.css';
 import type { AppProps } from 'next/app';
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Page>
