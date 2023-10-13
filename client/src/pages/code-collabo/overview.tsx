f3498db452fef758cf6eb18770fab50bc8714b19
Ifycode <marychord@gmail.com>
2023-10-13 02:01:43 +0100

    Make Page Head reusable component

--- client/src/pages/code-collabo/overview.tsx
+++ client/src/pages/code-collabo/overview.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head';
-import style from '@/styles/modules/overview.module.css';
-import { app } from '../../helpers/common';
+import { first_subcommunity, first_subcommunity_home } from '../../helpers/common';
+import PageHeadElement from '@/components/shared/PageHeadElement';
 
 export default function OverviewPage() {
-  const title = `${app.name} | Overview `;
+  const pageMetaDataObj = {
+    subcommunityName: first_subcommunity.name,
+    pageName: first_subcommunity_home.name,
+  }
   return (
     <>
-      <Head>
-        <title>{title}</title>
-      </Head>
+      <PageHeadElement { ...pageMetaDataObj } />
       <p><b>Overview page</b> (other) content here</p>
     </>
   );
--- client/src/pages/code-collabo/projects.tsx
+++ client/src/pages/code-collabo/projects.tsx
@@ -1,13 +1,14 @@
-import { app } from '@/helpers/common';
-import Head from 'next/head';
+import PageHeadElement from '@/components/shared/PageHeadElement';
+import { first_subcommunity, first_subcommunity_2nd_page, first_subcommunity_home } from '@/helpers/common';
 
 export default function ProjectsPage() {
-  const title = `${app.name} | Projects`;
+  const pageMetaDataObj = {
+    subcommunityName: first_subcommunity.name,
+    pageName: first_subcommunity_2nd_page.name,
+  }
   return (
     <>
-      <Head>
-        <title>{title}</title>
-      </Head>
+      <PageHeadElement { ...pageMetaDataObj } />
       <p><b>Projects page</b> (other) content here</p>
     </>
   );
