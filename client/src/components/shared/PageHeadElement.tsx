c4f5895c5e4e31488415af9e374471c9664ce502
Abraham Akpan <akpan@ogunlabs.com>
2023-11-03 07:56:12 +0100

    ⚡ Web app favicon icons

--- client/src/components/shared/PageHeadElement.tsx
+++ client/src/components/shared/PageHeadElement.tsx
@@ -1,12 +1,38 @@
 import { app, subcommunities } from '@/helpers/common';
 import Head from 'next/head';
 
-export default function PageHeadElement({ landingPage, subcommunityName, pageName }: { landingPage?: boolean; subcommunityName: string; pageName: string }) {
+export default function PageHeadElement({
+  landingPage,
+  subcommunityName,
+  pageName,
+}: {
+  landingPage?: boolean;
+  subcommunityName: string;
+  pageName: string;
+}) {
   const appName = landingPage ? app.name : subcommunityName;
   const PageTitle = `${appName} | ${pageName}`;
   return (
     <Head>
-        <title>{ PageTitle }</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <title>{PageTitle}</title>
     </Head>
   );
-};
+}
