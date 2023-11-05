09faf7f40dbb55b721bbb8c0430bc70e4f78f407
Ifycode <marychord@gmail.com>
2023-11-05 14:03:03 +0100

    Display all 3 different page structure in one place

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,13 +2,7 @@ import type { AppProps } from 'next/app';
 import '@/apps/shared/styles/imports.globals.css';
 import Page from '@/apps/shared/components/Page';
 
-export default function App({ Component, pageProps }: AppProps) {
-  console.log({ Component, pageProps });
-  return (
-    <></>
-    // <Page { Component, pageProps }></Page>
-    //   <Page>
-    //     <Component { ...pageProps } />
-    //  </Page>
-  );
+export default function App(appProps: AppProps) {
+  // console.log(appProps);
+  return <Page { ...appProps }></Page>;
 };
