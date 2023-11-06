d6b3e5d2910f7e8e7fadbb446326a520c99f439c
Ifycode <marychord@gmail.com>
2023-11-07 00:40:28 +0100

    Fix page breakage and styles for code-collabo pages, fix 404 page breakage

--- client/src/pages/code-collabo/projects.tsx
+++ client/src/pages/code-collabo/projects.tsx
@@ -1,20 +1,12 @@
 import { NextPageWithLayout } from '../_app';
 import { getCodeCollaboSubCommunityLayout } from '@/apps/code-collabo/components/AppPageLayout';
 
-import PageHeadElement from '@/apps/shared/components/PageHeadElement';
-import { first_subcommunity, first_subcommunity_2nd_page, first_subcommunity_home } from '@/apps/code-collabo/helpers/app';
-
 import projects from '@/apps/code-collabo/styles/modules/projects.module.css';
 
 const ProjectsPageCodeCollabo: NextPageWithLayout = () => {
-  const pageMetaDataObj = {
-    subcommunityName: first_subcommunity.name,
-    pageName: first_subcommunity_2nd_page.name,
-  }
   return (
     <>
-      <PageHeadElement { ...pageMetaDataObj } />
-      <h2 className={projects.test}>Projects Page Content CSS Module Test!</h2>
+      <h2 className={projects.test}>Dummy Page Content: CSS Module Test!</h2>
     </>
   );
 };
