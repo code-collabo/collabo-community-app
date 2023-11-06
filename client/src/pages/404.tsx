9c9cc1ffe469309855fecf940e7afb363c9d7223
Ifycode <marychord@gmail.com>
2023-11-07 00:40:28 +0100

    Fix page breakage and styles for code-collabo pages, fix 404 page breakage

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,14 +1,12 @@
 import PageHeadElement from '@/apps/shared/components/PageHeadElement';
-import { app } from '@/apps/code-collabo/helpers/app';
 
 export default function Custom404() {
-  const pageMetaDataObj = {
-    subcommunityName: app.name,
-    pageName: "404 - Page not found",
-  };
   return (
     <>
-      <PageHeadElement {...pageMetaDataObj} />
+      <PageHeadElement
+        pageTitle='404 - Page Not found'
+        faviconUrl=''
+      />
       <h1>404 - Custom Page Not Found</h1>
     </>
   );
