49dca1c7e51abc22a62537252b272676a7043584
Ifycode <marychord@gmail.com>
2023-11-07 01:38:05 +0100

    Fix metadata for other subcommunity overview and dynamically render page title

--- client/src/apps/other-subcommunity/components/AppPageStructure.tsx
+++ client/src/apps/other-subcommunity/components/AppPageStructure.tsx
@@ -1,10 +1,22 @@
+import { appInfo, urlStart } from '@/apps/other-subcommunity/helpers/appInfo';
+import PageHeadElement from '@/apps/shared/components/PageHeadElement';
+import { getPage } from '@/apps/shared/helpers/meta';
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 export default function PageStructure({ children }: { children: ReactNode}) {
+  let { pathname } = useRouter();
+  pathname === urlStart ? pathname =  `${urlStart}/overview` : pathname;
+  let { thisPage, pageTitle } = getPage(pathname, urlStart, appInfo.name);
   return (
     <>
+      <PageHeadElement
+        pageTitle={pageTitle}
+        faviconUrl=''
+      />
+
       <header>
-        <nav><b>OTHER SUBCOMMUNITY</b>nav menu (Basic)</nav>
+        <nav>{appInfo.name} [{thisPage}]</nav>
       </header>
       { children }
     </>
