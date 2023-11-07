17c41896f33a0f5d4eb77cb8290d0cc9661a6cec
Ifycode <marychord@gmail.com>
2023-11-07 01:51:02 +0100

    clean up shared components and helpers

--- client/src/apps/shared/components/PageHeadElement.tsx
+++ client/src/apps/shared/components/PageHeadElement.tsx
@@ -8,25 +8,3 @@ export default function PageHeadElement({ pageTitle, faviconUrl }: { pageTitle: 
     </Head>
   );
 };
-
-// import { app, subcommunities } from '@/apps/code-collabo/helpers/appInfo';
-
-// export default function PageHeadElement({ landingPage, subcommunityName, pageName }: { landingPage?: boolean; subcommunityName: string; pageName: string }) {
-//   // const appName = landingPage ? app.name : subcommunityName;
-//   // const PageTitle = `${appName} | ${pageName}`;
-//   return (
-//     <Head>
-//         <link rel="icon" type="image/x-icon" href="/code-collabo/favicon.ico" />
-//         {/* <title>{ PageTitle }</title> */}
-//     </Head>
-//     // <Head>
-//     //     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-//     //     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-//     //     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-//     //     <link rel="manifest" href="/site.webmanifest" />
-//     //     <title>{ PageTitle }</title>
-//     // </Head>
-//   );
-// };
-
-
