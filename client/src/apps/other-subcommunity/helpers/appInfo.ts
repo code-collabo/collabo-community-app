d2a7ebea0da1927aa67fd842c7ed451fe71be86f
Ifycode <marychord@gmail.com>
2023-11-07 01:38:05 +0100

    Fix metadata for other subcommunity overview and dynamically render page title

---
+++ client/src/apps/other-subcommunity/helpers/appInfo.ts
@@ -0,0 +1,27 @@
+import { capitalizeAllFirstLetters, stringToUrlStart } from "@/apps/shared/helpers/transform";
+
+const appName = 'other subcommunity';
+
+const urlStart = stringToUrlStart(appName);
+
+const page = {
+  1: 'overview',
+};
+
+const appInfo: any = {
+  id: 1,
+  name: capitalizeAllFirstLetters(appName),
+  pages: {
+    1: {
+      name: page[1],
+      route: `${urlStart}/${page[1]}`,
+    },
+  }
+};
+
+console.log(appInfo);
+
+export {
+  appInfo,
+  urlStart,
+};
--- client/src/pages/other-subcommunity/overview.tsx
+++ client/src/pages/other-subcommunity/overview.tsx
@@ -1,20 +1,12 @@
 import { NextPageWithLayout } from '../_app';
 import { getOtherSubCommunityLayout } from '@/apps/other-subcommunity/components/AppPageLayout';
 
-import PageHeadElement from '@/apps/shared/components/PageHeadElement';
-import { first_subcommunity, first_subcommunity_home } from '@/apps/code-collabo/helpers/app';
-
 import overview from '@/apps/other-subcommunity/styles/modules/overview.module.css';
 
 const OverviewPageOther: NextPageWithLayout = () => {
-  const pageMetaDataObj = {
-    subcommunityName: first_subcommunity.name,
-    pageName: first_subcommunity_home.name,
-  }
   return (
     <>
-      <PageHeadElement { ...pageMetaDataObj } />
-      <h2 className={overview.test}>Overview CSS Page Content Module Test!</h2>
+      <h2 className={overview.test}>Dummy Page Content: CSS Module Test!</h2>
     </>
   );
 };
