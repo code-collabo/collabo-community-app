61b13602c7b43d64a6038220262698f66917c897
Ifycode <marychord@gmail.com>
2023-11-07 01:18:36 +0100

    Fix metadata for collabo community app home and dynamically render page title

--- client/src/apps/code-collabo/components/AppPageStructure.tsx
+++ client/src/apps/code-collabo/components/AppPageStructure.tsx
@@ -2,12 +2,12 @@ import PageHeadElement from '@/apps/shared/components/PageHeadElement';
 import { getPage } from '@/apps/shared/helpers/meta';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
-import { urlStart } from '../helpers/appInfo';
+import { appInfo, urlStart } from '@/apps/code-collabo/helpers/appInfo';
 
 export default function PageStructure({ children }: { children: ReactNode}) {
   let { pathname } = useRouter();
   pathname === urlStart ? pathname =  `${urlStart}/overview` : pathname;
-  let { thisPage, pageTitle } = getPage(pathname, urlStart);
+  let { thisPage, pageTitle } = getPage(pathname, urlStart, appInfo.name);
 
   return (
     <>
--- client/src/apps/shared/helpers/meta.ts
+++ client/src/apps/shared/helpers/meta.ts
@@ -1,13 +1,12 @@
-import { appInfo } from '@/apps/code-collabo/helpers/appInfo';
 import { capitalizeAllFirstLetters } from './transform';
 
 const toPageTitle = (pageName: string, appName: string) => { //not exported only used in this file for now
     return `${pageName} | ${appName}`;
 };
 
-const getPage = (pathname: string, urlStart: string) => {
+const getPage = (pathname: string, urlStart: string, appName: string) => {
     const thisPage = capitalizeAllFirstLetters(pathname.replace(`${urlStart}/`, ''))
-    const pageTitle = toPageTitle(thisPage, appInfo.name);
+    const pageTitle = toPageTitle(thisPage, appName);
     return {
         thisPage,
         pageTitle,
